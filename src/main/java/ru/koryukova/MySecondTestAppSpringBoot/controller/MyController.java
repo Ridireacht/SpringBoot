edefc83f8e44fc2f642be4e9c7c3430f1687ca50
Ridireacht <ridiredire@gmail.com>
2023-10-28 00:25:06 +0300

    Fix validation

--- src/main/java/ru/koryukova/MySecondTestAppSpringBoot/controller/MyController.java
+++ src/main/java/ru/koryukova/MySecondTestAppSpringBoot/controller/MyController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,6 @@ import ru.koryukova.MySecondTestAppSpringBoot.service.ValidationService;
 
 
 @RestController
-@Validated
 public class MyController {
 
   private final ValidationService validationService;
