ee62e695462515f87c3e1a86b0241be938fc330c
Ridireacht <ridiredire@gmail.com>
2023-11-05 22:21:23 +0300

    Rename main method in service

--- Service1/src/test/java/ru/koryukova/MySecondTestAppSpringBoot/FirstServiceApplicationTests.java
+++ Service1/src/test/java/ru/koryukova/MySecondTestAppSpringBoot/FirstServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MySecondTestAppSpringBootApplicationTests {
+class FirstServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
