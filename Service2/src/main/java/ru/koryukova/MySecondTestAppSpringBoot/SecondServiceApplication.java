169525d43010d0bd12cc2b41f833aee46efd7214
Ridireacht <ridiredire@gmail.com>
2023-11-05 22:16:54 +0300

    Rename main method

--- Service2/src/main/java/ru/koryukova/MySecondTestAppSpringBoot/SecondServiceApplication.java
+++ Service2/src/main/java/ru/koryukova/MySecondTestAppSpringBoot/SecondServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MySecondTestAppSpringBootApplication {
+public class SecondServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySecondTestAppSpringBootApplication.class, args);
+		SpringApplication.run(SecondServiceApplication.class, args);
 	}
 
 }
