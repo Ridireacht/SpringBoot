93428dc7e4ea032f62a33c7510302843cfa0922f
Ridireacht <ridiredire@gmail.com>
2023-11-13 23:20:29 +0300

    Check leap year

--- Service1/src/main/java/ru/koryukova/FirstService/service/AnnualBonusServiceImpl.java
+++ Service1/src/main/java/ru/koryukova/FirstService/service/AnnualBonusServiceImpl.java
@@ -1,11 +1,24 @@
 package ru.koryukova.FirstService.service;
 
+import java.time.Year;
+import java.util.GregorianCalendar;
 import ru.koryukova.FirstService.model.Positions;
 
 public class AnnualBonusServiceImpl implements AnnualBonusService {
 
   @Override
   public double calculate(Positions positions, double salary, double bonus, int workDays) {
-    return salary * bonus * 365 * positions.getPositionCoefficient() / workDays;
+
+    int daysInYear;
+    Year thisYear = Year.now();
+    GregorianCalendar calendar = new GregorianCalendar();
+
+    if (calendar.isLeapYear(Integer.parseInt(thisYear.toString()))) {
+      daysInYear = 366;
+    } else {
+      daysInYear = 365;
+    }
+
+    return salary * bonus * daysInYear * positions.getPositionCoefficient() / workDays;
   }
 }
