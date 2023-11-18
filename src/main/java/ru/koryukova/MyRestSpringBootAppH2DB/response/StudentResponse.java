f1f9049307d82bde40b67510277b534e358b95b8
Ridireacht <ridiredire@gmail.com>
2023-11-18 16:18:32 +0300

    Fix

--- src/main/java/ru/koryukova/MyRestSpringBootAppH2DB/response/StudentResponse.java
+++ src/main/java/ru/koryukova/MyRestSpringBootAppH2DB/response/StudentResponse.java
@@ -3,8 +3,8 @@ package ru.koryukova.MyRestSpringBootAppH2DB.response;
 import ru.koryukova.MyRestSpringBootAppH2DB.entity.Student;
 
 public class StudentResponse {
-    private Student student;
-    private boolean success;
+    public Student student;
+    public boolean success;
 
   public StudentResponse(Student student, boolean success) {
     this.student = student;
