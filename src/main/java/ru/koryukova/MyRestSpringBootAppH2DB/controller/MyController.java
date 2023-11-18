d92a3758d367853cb5964c70196da5efcf113249
Ridireacht <ridiredire@gmail.com>
2023-11-18 14:49:35 +0300

    Add controller

---
+++ src/main/java/ru/koryukova/MyRestSpringBootAppH2DB/controller/MyController.java
@@ -0,0 +1,46 @@
+package ru.koryukova.MyRestSpringBootAppH2DB.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+  @Autowired
+  private  StudentService studentService;
+
+  @GetMapping("/students")
+  public List<Student> showAllStudents() {
+    List<Student> allStudents = studentService.getAllStudents();
+    return allStudents;
+  }
+
+  @GetMapping("/students/{id}")
+  public Student getStudent(@PathVariable("id") int id) {
+    return studentService.getStudent(id);
+  }
+
+  @PostMapping("/students")
+  public Student saveStudent(@RequestBody Student student) {
+    return studentService.saveStudent(student);
+  }
+
+  @PutMapping("/students")
+  public Student updateStudent(@RequestBody Student student) {
+    studentService.saveStudent(student);
+    return student;
+  }
+
+  @DeleteMapping("/students/{id}")
+  public void deleteStudent(@PathVariable("id") int id) {
+    studentService.deleteStudent(id);
+  }
+}
