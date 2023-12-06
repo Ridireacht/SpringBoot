a0ee286b12fd4d9f73d9ebc2387ae7a347cd21b2
Ridireacht <ridiredire@gmail.com>
2023-12-06 22:17:48 +0300

    Initial commit

---
+++ src/main/java/com/example/testthymeleafapp/TestThymeleafAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.testthymeleafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestThymeleafAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestThymeleafAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testthymeleafapp/TestThymeleafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testthymeleafapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThymeleafAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
